8157a8e4dc7d0225d2d171ae1601fe89908fe0df
Shashi Jangra <scar@Shashis-MacBook-Pro.local>
2019-09-14 11:57:15 +0530

    added policy based data structure

--- AwesomeQs.md
+++ AwesomeQs.md
@@ -3,3 +3,6 @@
 
 #Greedy:
 	- https://www.spoj.com/problems/GCJ101BB/
+
+#Data Structures:
+	- https://leetcode.com/problems/count-of-smaller-numbers-after-self/
--- template.cpp
+++ template.cpp
@@ -1,17 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+// include <ext/pb_ds/assoc_container.hpp>
+// include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
 #define ll long long
+// typedef tree <ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> set_t;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define vi vector<ll>
 #define vvi vector<vi>
 #define Q queue<ll>
-#define DQ deque<ll> 
+#define DQ deque<ll>
+#define P_Q priority_queue<ll>
+#define p_q priority_queue<ll,vi,greater<ll>>
 #define pb push_back
 #define pf push_front
 #define popf pop_front
 #define popb pop_back
-#define mp make_pair
-#define mt make_tuple
 #define f first
 #define s second
 #define ub upper_bound
