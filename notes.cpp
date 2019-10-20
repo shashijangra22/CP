ac4e34d0497a2cc9c9980ac9fc244c770037ba32
Shashi Jangra <shashijangra@live.com>
2019-10-21 01:42:04 +0530

    binary exp

--- notes.cpp
+++ notes.cpp
@@ -193,6 +193,18 @@ loop(i,2,N,1){
 	}
 }
 
+// binary exponentiation
+
+modPow(a,b,m){
+	ll res=1
+	while(b){
+		if (b&1) res=(res*a)%m;
+		a=(a*a)%m;
+		b>>=1;
+	}
+	return res%m;
+}
+
 // DSU
 
 vector<ll> parent(n); // initially self
