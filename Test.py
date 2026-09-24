8c6a92e42860edcfa464e75e997282d37232fe20
Shashi Jangra <10132780+shashijangra22@users.noreply.github.com>
2026-09-24 20:29:09 +0530

    updating script and fixing macos interpretor path issue

--- Test.py
+++ Test.py
@@ -9,75 +9,98 @@ ap.add_argument(
 args = vars(ap.parse_args())
 
 def getUrlFromFile():
-    f = open(args["file"], "r")
-    URL = f.read().split('\n')[0]
-    f.close()
-    pos = URL.find("http")
-    if pos != -1:
-        return URL[pos:]
+    # Improved to find the URL anywhere in the first few lines, 
+    # even if preceded by comment characters like // or #
+    with open(args["file"], "r") as f:
+        for line in f:
+            pos = line.find("http")
+            if pos != -1:
+                # Extract URL and drop any trailing comments or spaces
+                return line[pos:].split()[0]
     return None
 
-
 args["url"] = getUrlFromFile()
+if not args["url"]:
+    print("[Error]: Could not find a valid URL in the source file.")
+    exit()
 
-
-def getTests():
+def getTests(dirName):
     inputs, outputs = [], []
-    res = requests.get(args["url"]).text
-    soup = bs(res, 'html.parser')
+    
+    # Added a User-Agent to bypass Codeforces' basic bot protection (403 Forbidden)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    }
+    res = requests.get(args["url"], headers=headers)
+    if res.status_code != 200:
+        print(f"[Error]: Failed to fetch page. Status code: {res.status_code}")
+        return
+        
+    soup = bs(res.text, 'html.parser')
 
-    def scrapeInputText(div):
-        ans = [i.text for i in div.find('pre').children]
-        return "\n".join(ans).strip()
-
-    def scrapeOutputText(div):
-        return div.find('pre').text.strip()
+    def scrapeText(div):
+        pre = div.find('pre')
+        if not pre: return ""
+        
+        # Codeforces now uses <br> or <div> for line breaks. 
+        # Replace <br> with newlines explicitly.
+        for br in pre.find_all('br'):
+            br.replace_with('\n')
+            
+        # get_text with a newline separator handles their block elements cleanly
+        return pre.get_text(separator='\n').strip()
 
     inputDiv = soup.find_all('div', {"class": "input"})
     outputDiv = soup.find_all('div', {"class": "output"})
-    for x in range(len(inputDiv)):
-        inputs.append(scrapeInputText(inputDiv[x]))
-        outputs.append(scrapeOutputText(outputDiv[x]))
-    for ind in range(len(inputs)):
-        with open("test-"+str(ind+1)+".in", "w") as f:
-            f.write(inputs[ind])
-        with open("test-"+str(ind+1)+".out", "w") as f:
-            f.write(outputs[ind])
-    os.chdir("..")
+    
+    if not inputDiv:
+        print("[Warning]: No test cases found. Check the URL or CF bot protection.")
 
+    for x in range(len(inputDiv)):
+        inputs.append(scrapeText(inputDiv[x]))
+        outputs.append(scrapeText(outputDiv[x]))
+        
+    for ind in range(len(inputs)):
+        # Replaced os.chdir() with direct pathing to prevent directory state bugs
+        with open(os.path.join(dirName, f"test-{ind+1}.in"), "w") as f:
+            f.write(inputs[ind])
+        with open(os.path.join(dirName, f"test-{ind+1}.out"), "w") as f:
+            f.write(outputs[ind])
 
 try:
-    dirName = args["url"].split('/')[-1]+"test"
-except:
+    # Cleaner directory name generation, stripping trailing slashes
+    dirName = args["url"].rstrip('/').split('/')[-1] + "_test"
+except Exception:
     print("Invalid URL!")
     exit()
 
 if os.path.exists(dirName):
-    print("Directory already exists! Reading from Directory...")
+    print(f"Directory '{dirName}' already exists! Reading from Directory...")
 else:
     print("Fetching sample tests from server...")
     os.mkdir(dirName)
-    os.chdir(dirName)
-    getTests()
+    getTests(dirName)
 
-tests = sorted([file.split('.')[0]
-               for file in os.listdir(dirName) if 'in' in file])
+# Fixed test case sorting so test-10 runs after test-9, not after test-1
+tests = [file.split('.')[0] for file in os.listdir(dirName) if file.endswith('.in')]
+tests.sort(key=lambda x: int(x.split('-')[1]))
 
-ext = args["file"][args["file"].find('.'):]
-
+ext = os.path.splitext(args["file"])[1]
 
 def getCommand(ext, test):
     if ext == ".cpp":
-        return "./a.out" + " < " + dirName + "/" + test + ".in > " + dirName + "/my" + test + ".out"
+        return f"./a.out < {dirName}/{test}.in > {dirName}/my{test}.out"
     elif ext == ".py":
-        return "python3 " + args["file"] + " < " + dirName + "/" + test + ".in > " + dirName + "/my" + test + ".out"
+        return f"python3 {args['file']} < {dirName}/{test}.in > {dirName}/my{test}.out"
+    return ""
 
-
-commands = ""
 if ext == ".cpp":
-    command = "g++ " + args["file"] + " -std=c++17"
+    command = f"g++ {args['file']} -std=c++17"
     print("Compiling source code...")
-    os.system(command)
+    # Added error check to prevent running outdated binaries if compilation fails
+    if os.system(command) != 0:
+        print("[Error]: Compilation failed!")
+        exit()
 elif ext == ".py":
     pass
 else:
@@ -85,22 +108,24 @@ else:
     exit()
 
 for test in tests:
-    print("\nRunning", test, "=>", end=" ")
+    print(f"\nRunning {test} => ", end="")
     command = getCommand(ext, test)
-    op = os.system(command)
-    expectedFile = open(dirName+'/'+test+'.out', "r")
-    myresultFile = open(dirName+'/my'+test+'.out', "r")
-    expected = expectedFile.read().strip()
-    myresult = myresultFile.read().strip()
-    if (expected == myresult):
-        print("Passed!")
-    else:
-        print("Failed !!!")
-        print("\nInput\n")
-        print(open(dirName+'/'+test+'.in').read().strip())
-        print("\nExpected Output\n")
-        print(expected)
-        print("\nMy Output\n")
-        print(myresult)
-    expectedFile.close()
-    myresultFile.close()
+    os.system(command)
+    
+    with open(f"{dirName}/{test}.out", "r") as expectedFile, \
+         open(f"{dirName}/my{test}.out", "r") as myresultFile, \
+         open(f"{dirName}/{test}.in", "r") as inputFile:
+             
+        expected = expectedFile.read().strip()
+        myresult = myresultFile.read().strip()
+        
+        if expected == myresult:
+            print("Passed!")
+        else:
+            print("Failed !!!")
+            print("\n--- Input ---")
+            print(inputFile.read().strip())
+            print("\n--- Expected Output ---")
+            print(expected)
+            print("\n--- My Output ---")
+            print(myresult)
